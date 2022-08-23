0027289e87ff58a02daa75cecc471f42a530f538
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-08-23 13:40:47 +0200

    Lägger till response status json på servern

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const express
-
-
 const server = express();
+server.use(express.json())
+
+server.get("/", (req, res) => {
+    headers={"cache-control": "no-cache"}
+    body={"status": "its working"}
+    res.status(200).json(body)
+})
+
 
 server.listen(8080, () => {
     console.log('Port 8080 ready')
