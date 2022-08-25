20f893fdeb1edd4031ed31f1026e6d18b76c766a
Robert Lockett <lockettr@hotmail.com>
2022-08-25 11:30:07 +0200

    klar med favicon

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const routes = require('./routes/user.js')
+var favicon = require('serve-favicon')
+var path = require('path')
 
 const app = express()
 app.use(express.json())
 app.use('/user', routes)
+app.use(favicon(path.join(__dirname, 'public', 'Rocket-icon.ico')))
 
 app.get("/", (req, res) => {
     headers={"cache-control": "no-cache"}
