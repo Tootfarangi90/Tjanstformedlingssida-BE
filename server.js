8f1f95dee4e96c40a27b29ac37767770c6ba442c
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-08-29 12:24:10 +0200

    Connected to database, and created a .env file with the url. Installed dotenv and mongoose modules.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
-const routes = require('./routes/user.js')
-const cors = require('cors')
-
-
 const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+const routes = require('./routes/user.js')
+
+
+mongoose.connect('process.env.DATABASE_URL', () => {
+    console.log("Database is connected")
+})
+
+
 app.use(cors())
 app.use(express.json())
 app.use('/', routes)
