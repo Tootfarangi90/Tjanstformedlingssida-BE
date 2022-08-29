68efba78c4da17ed462fd3974549a4aa02f7cfe9
Ayda Sholani <shoolani@gmail.com>
2022-08-29 12:23:29 +0200

    login for users

--- routes/user.js
+++ routes/user.js
@@ -16,6 +16,42 @@ router.post('/register', (req, res, next) => {
     res.status(200).send(users)
 })
 
+router.post('/login', async (req,res, next) => {
+try {
+    const { email, password } = req.body
+    console.log(req.body)
+
+    if(!(email && password)) {
+        res.status(400).json({message: "All inputs are required"})
+        return
+    }
+    const user = await users.findOne({email})
+    console.log(user)
+    console.log(user.email)
+    if(!email) {
+        res.json({status: 404, message: "user not found"})
+        return
+    }
+    console.log(user.password)
+
+    const passwordValidation = await password === user.password
+    if(!passwordValidation) {
+        res.json({status: 401, message: "Invalid password"})
+        return
+    }    
+    if(user && passwordValidation) {
+        res.json({status: 200, message: "Welcome"})
+        return
+    }
+    
+} catch (error) {
+    console.log('Error reason: ' + error)
+    res.json({message: error})
+    return 
+}
+
+})
+
 router.get('/getusers', (req, res) => {
     res.status(200).json(users)
 })
