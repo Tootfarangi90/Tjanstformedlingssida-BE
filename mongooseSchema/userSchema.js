77e37a63761fc2aef315ec346265a8212af902c8
Ayda Sholani <shoolani@gmail.com>
2022-08-31 10:39:21 +0200

    tog bort birthday

--- mongooseSchema/userSchema.js
+++ mongooseSchema/userSchema.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
     lastname: { type: String, required: true }, 
     email: { type: String, required: true },
     password: { type: String, required: true },
-    birthday: { type: String, required: true },
     occupation: { type: String, required: true }
 
 })
