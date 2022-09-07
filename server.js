23fbdeafbdbc12d91d8f1418ea7df2f045bc8c0b
Robert Lockett <lockettr@hotmail.com>
2022-09-07 10:54:42 +0200

    commit innan pull på main

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const app = express();
 const routes = require ('./routes');
 var favicon = require('serve-favicon')
 var path = require('path')
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 
-const app = express();
+
+
 const port = process.env.PORT || 5000;
+
+
+
+
+
+
 app.use(express.static('public'))
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-
-
-
-
-
 app.use(express.json())
 app.use('/', routes);
 app.listen(port, () => console.log(`Server started on port ${port}`))
